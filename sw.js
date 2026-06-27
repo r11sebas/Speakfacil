66b3cb6d598a1e7ec32a72bd99de58766e8575fa
r11sebas <r11jsebas@gmail.com>
2026-06-27 16:10:45 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE_NAME = 'mokitalk-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/moki.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
